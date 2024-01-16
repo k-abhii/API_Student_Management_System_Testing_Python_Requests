798f32db015818c77694c214e4971867998e76ef
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2024-01-16 12:10:08 +0530

    add tech details

--- TestCases/test_end_to_end.py
+++ TestCases/test_end_to_end.py
@@ -7,4 +7,14 @@ def test_add_data():
     request_json = json.loads(file.read())
     response = requests.post(APP_URL,request_json)
     print(response.text)
+    id = jsonpath.jsonpath(response.json(),'id')
+    print(id[0])
+    tech_url = "https://thetestingworldapi.com/api/technicalskills"
+    f = open("C:/Users/hp/PycharmProjects/API_Student_Management_System/TestCases/TechDetails.json", "r")
+    request_js = json.loads(f.read())
+    response = requests.post(tech_url,request_js)
+    print(response.text)
+
+
+
 
