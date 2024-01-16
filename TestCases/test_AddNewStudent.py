3be414bfddd34023f0c1ae590b9bbe328c7c05fe
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2024-01-16 09:12:45 +0530

    Update Details using put

--- TestCases/test_AddNewStudent.py
+++ TestCases/test_AddNewStudent.py
@@ -17,3 +17,16 @@ def test_get_student_data():
     print(id)
     assert id[0] ==10056912
 
+# git remote add origin https://github.com/k-abhii/API_Student_Management_System_Testing_Python_Requests.git
+# git branch -M main
+# git push -u origin main
+
+def test_update_details():
+    API_URL = "https://thetestingworldapi.com/api/studentsDetails/10056912"
+    f = open("C:/Users/hp/PycharmProjects/API_Student_Management_System/TestCases/PutRequest.json")
+    request_json = json.loads(f.read())
+    response = requests.put(API_URL,request_json)
+    print(response.text)
+    
+
+
